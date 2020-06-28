83ffa461e702d886e878f59b21a93ac40d243251
aksharans <aksharan@gmail.com>
2020-06-28 14:24:54 -0700

    csv, individual major pages

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, url_for
+import pandas as pd
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,6 +8,13 @@ def index():
     "Bioengineering", "Mechanical Engineering", "Aerospace Engineering", "Chemical Engineering",
     "Nano Engineering", "Structural Engineering"]
     return render_template('index.html', majors=majors_list)
+
+@app.route('/<major>')
+def welcome(major):
+
+    course_list = ["CSE", "ECE"]
+    data = pd.read_csv('./data/CSE_courses.csv').to_html()
+    return render_template('page.html', major=major, courses=course_list, data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)

52377d2877e4017884064b1118d7a9aec7b38d1b
aksharans <aksharan@gmail.com>
2020-07-02 14:58:59 -0700

    added table and included generate pdf functionality

--- static/js/function.js
+++ static/js/function.js
@@ -6,3 +6,12 @@ function toggle(course) {
       x.style.display = "none";
     }
   }
+
+  function createPDF(){
+    var data = document.getElementById('t2').innerHTML;
+    var win = window.open('', '', height=400,width=600);
+    win.document.write('<link rel="stylesheet" href="../static/css/style.css" />');
+    win.document.write(data);
+    win.document.close(); 
+    //win.print();
+  }
